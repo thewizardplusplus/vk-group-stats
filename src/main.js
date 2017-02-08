d4a0e9749497f5a1e3d0744d9c12ba1d8875a3a1
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-02-09 02:31:55 +0300

    Parse a request body

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import {logger, http_logger} from './utils/logger'
+import body_parser from 'body-parser'
 import {user_router} from './routers/user_router'
 
 const app = express()
 app.use(http_logger)
+app.use(body_parser.json())
 app.use(user_router)
 
 const port = process.env.VK_GROUP_STATS_PORT || 3000
