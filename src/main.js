a4f0d821733dc044f37cfbde544be95e8efc214d
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-02-09 11:42:00 +0300

    Validate user router parameters

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import {logger, http_logger} from './utils/logger'
 import body_parser from 'body-parser'
+import express_validator from 'express-validator'
 import {user_router} from './routers/user_router'
 
 const app = express()
 app.use(http_logger)
 app.use(body_parser.json())
+app.use(express_validator())
 app.use(user_router)
 
 const port = process.env.VK_GROUP_STATS_PORT || 3000
--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -1,24 +1,68 @@
 import express from 'express'
 
+function validate_request_body(request, response, next_handler) {
+  request.checkBody({
+    'email': {
+      notEmpty: {
+        errorMessage: 'parameter is required',
+      },
+      isEmail: {
+        errorMessage: 'parameter must be an email',
+      },
+    },
+    'password': {
+      notEmpty: {
+        errorMessage: 'parameter is required',
+      },
+    },
+  })
+
+  request.getValidationResult().then(result => {
+    if (result.isEmpty()) {
+      next_handler()
+    } else {
+      response.status(400).json({
+        errors: result.array(),
+      })
+    }
+  })
+}
+
 const all_users_router = express.Router()
 all_users_router.route('/users')
   .get((request, response) => {
     response.json({})
   })
-  .post((request, response) => {
+  .post(validate_request_body, (request, response) => {
     response.json({
       user: request.body,
     })
   })
 
 const one_user_router = express.Router()
+one_user_router.param('user_id', (request, response, next_handler) => {
+  request.checkParams('user_id', 'parameter is required').notEmpty()
+  request
+    .checkParams('user_id', 'parameter must be a MongoDB ObjectId')
+    .isMongoId()
+
+  request.getValidationResult().then(result => {
+    if (result.isEmpty()) {
+      next_handler()
+    } else {
+      response.status(400).json({
+        errors: result.array(),
+      })
+    }
+  })
+})
 one_user_router.route('/users/:user_id')
   .get((request, response) => {
     response.json({
       user_id: request.params.user_id,
     })
   })
-  .put((request, response) => {
+  .put(validate_request_body, (request, response) => {
     response.json({
       user_id: request.params.user_id,
       user: request.body,
