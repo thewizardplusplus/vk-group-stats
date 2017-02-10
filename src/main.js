141d66bd7138276588497a79b9aba15b441cca69
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-02-10 13:02:17 +0300

    Connect to MongoDB

--- src/main.js
+++ src/main.js
@@ -1,18 +1,33 @@
-import express from 'express'
+import mongoose from 'mongoose'
 import {logger, http_logger} from './utils/logger'
+import express from 'express'
 import body_parser from 'body-parser'
 import express_validator from 'express-validator'
 import {user_router} from './routers/user_router'
 import {error_handler} from './utils/errors'
 
-const app = express()
-app.use(http_logger)
-app.use(body_parser.json())
-app.use(express_validator())
-app.use(user_router)
-app.use(error_handler)
+mongoose.Promise = global.Promise
+mongoose
+  .connect(
+    process.env.VK_GROUP_STATS_MONGODB_HOST || 'localhost',
+    process.env.VK_GROUP_STATS_MONGODB_DATABASE || 'vk-group-stats',
+    process.env.VK_GROUP_STATS_MONGODB_PORT || 27017
+  )
+  .then(() => {
+    logger.info('app connected to MongoDB')
 
-const port = process.env.VK_GROUP_STATS_PORT || 3000
-app.listen(port, () => {
-  logger.info(`app listening on port ${port}`)
-})
+    const app = express()
+    app.use(http_logger)
+    app.use(body_parser.json())
+    app.use(express_validator())
+    app.use(user_router)
+    app.use(error_handler)
+
+    const port = process.env.VK_GROUP_STATS_SERVER_PORT || 3000
+    app.listen(port, () => {
+      logger.info(`app listening on port ${port}`)
+    })
+  })
+  .catch(error => {
+    logger.error(`app can't connect to MongoDB: ${util.inspect(error)}`)
+  })
