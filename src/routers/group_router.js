3221697ea990537206348b20c3a8cf29fe163433
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-02-16 11:04:35 +0300

    Implement group routes

--- src/routers/group_router.js
+++ src/routers/group_router.js
@@ -1,5 +1,6 @@
 import {check_authentication, validate_request} from '../utils/errors'
 import express from 'express'
+import {group_model} from '../models/group_model'
 
 function validate_request_body(request, response, next_handler) {
   request.checkBody({
@@ -15,13 +16,26 @@ function validate_request_body(request, response, next_handler) {
 
 const all_groups_router = express.Router()
 all_groups_router.route('/groups')
-  .get(check_authentication, (request, response) => {
-    response.json({})
+  .get(check_authentication, (request, response, next_handler) => {
+    group_model
+      .find({
+        user_id: request.user.id,
+      })
+      .then(groups => response.json(groups))
+      .catch(next_handler)
   })
-  .post(check_authentication, validate_request_body, (request, response) => {
-    response.json({
-      group: request.body,
-    })
+  .post(check_authentication, validate_request_body, (
+    request,
+    response,
+    next_handler
+  ) => {
+    group_model
+      .create({
+        user_id: request.user.id,
+        screen_name: request.body.screen_name,
+      })
+      .then(group => response.json(group))
+      .catch(next_handler)
   })
 
 const one_group_router = express.Router()
@@ -34,10 +48,13 @@ one_group_router.param('group_id', (request, response, next_handler) => {
   validate_request(request, next_handler)
 })
 one_group_router.route('/groups/:group_id')
-  .delete(check_authentication, (request, response) => {
-    response.json({
-      group_id: request.params.group_id,
-    })
+  .delete(check_authentication, (request, response, next_handler) => {
+    group_model
+      .remove({
+        _id: request.params.group_id,
+      })
+      .then(() => response.json(true))
+      .catch(next_handler)
   })
 
 export const group_router = express.Router()
