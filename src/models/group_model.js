f2d49bf4c4f4bdb0f2407306fcda2900cc66002c
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-02-18 01:47:22 +0300

    Add group_schema.update_counter() method

--- src/models/group_model.js
+++ src/models/group_model.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose'
+import {logger} from '../utils/logger'
+import {default_error} from '../utils/errors'
+import {counter_model} from '../models/counter_model'
+import {vk_api} from '../utils/vk_api'
 
-export const group_model = mongoose.model('group', new mongoose.Schema({
+const group_schema = new mongoose.Schema({
   user_id: {
     type: mongoose.Schema.Types.ObjectId,
     required: [true, 'parameter is required'],
@@ -9,4 +13,30 @@ export const group_model = mongoose.model('group', new mongoose.Schema({
     type: String,
     required: [true, 'parameter is required'],
   },
-}))
+})
+group_schema.methods.update_counter = function(done_handler) {
+  vk_api.request('groups.getById', {
+    'group_id': this.screen_name,
+    'fields': 'members_count',
+  }, response => {
+    logger.info(`group ${this.id} requested VK API`)
+
+    if (typeof response.error !== 'undefined') {
+      done_handler(new default_error(
+        `VK API error: "${response.error.error_msg}"`
+      ))
+
+      return
+    }
+
+    counter_model
+      .create({
+        group_id: this.id,
+        value: response.response[0].members_count,
+      })
+      .then(counter => done_handler(null, counter))
+      .catch(done_handler)
+  })
+}
+
+export const group_model = mongoose.model('group', group_schema)
