04645107f8425eacb30bbdae7f0dc7b467c66906
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-02-18 18:42:21 +0300

    Correct group_schema.update_counter() method

--- src/models/group_model.js
+++ src/models/group_model.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose'
 import {logger} from '../utils/logger'
-import {default_error} from '../utils/errors'
 import {counter_model} from '../models/counter_model'
-import {vk_api} from '../utils/vk_api'
+import {vk_api_client} from '../utils/vk_api'
 
 const group_schema = new mongoose.Schema({
   user_id: {
@@ -15,28 +14,23 @@ const group_schema = new mongoose.Schema({
   },
 })
 group_schema.methods.update_counter = function(done_handler) {
-  vk_api.request('groups.getById', {
-    'group_id': this.screen_name,
-    'fields': 'members_count',
-  }, response => {
-    logger.info(`group ${this.id} requested VK API`)
+  vk_api_client
+    .call('groups.getById', {
+      group_id: this.screen_name,
+      fields: 'members_count',
+    })
+    .then(response => {
+      logger.info(`group ${this.id} requested VK API`)
 
-    if (typeof response.error !== 'undefined') {
-      done_handler(new default_error(
-        `VK API error: "${response.error.error_msg}"`
-      ))
-
-      return
-    }
-
-    counter_model
-      .create({
-        group_id: this.id,
-        value: response.response[0].members_count,
-      })
-      .then(counter => done_handler(null, counter))
-      .catch(done_handler)
-  })
+      counter_model
+        .create({
+          group_id: this.id,
+          value: response[0].members_count,
+        })
+        .then(counter => done_handler(null, counter))
+        .catch(done_handler)
+    })
+    .catch(done_handler)
 }
 
 export const group_model = mongoose.model('group', group_schema)
