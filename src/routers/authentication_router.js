08a4a0509e03dcdcf581abfe14c6f90fb27838d9
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-02-28 03:28:09 +0300

    Correct the authentication router

--- src/routers/authentication_router.js
+++ src/routers/authentication_router.js
@@ -7,11 +7,11 @@ authentication_router
   .get('/authentication/vk', passport.authenticate('vkontakte'))
   .get('/authentication/vk/callback', passport.authenticate('vkontakte', {
     successRedirect: '/',
-    failureRedirect: '/login',
+    failureRedirect: '/',
   }))
   .get('/logout', (request, response) => {
     logger.info(`user ${request.user.id} logged out of the app`)
     request.logout()
 
-    response.redirect('/login')
+    response.redirect('/')
   })
