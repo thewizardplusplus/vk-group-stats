374daecc55ffe95a1738f3fb03cdb3925c152328
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-03-01 01:26:23 +0300

    Set up a base template

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
 import Icon from '../icon/Icon'
 import './app.css'
 
 export default class App extends React.Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <div className="app">
-          <div className="app-header">
-            <h2>Статистика сообществ ВКонтакте</h2>
-          </div>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <div>
+          <AppBar
+            title="Статистика сообществ ВКонтакте"
+            showMenuIconButton={false} />
+
           <p className="app-intro">
             Hello, world! <Icon name="cake" />
           </p>
