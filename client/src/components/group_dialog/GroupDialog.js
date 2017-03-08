36fe3edfa0ca6f36ca629a7fc164766e443790e7
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-03-08 17:51:46 +0300

    Add a group form to the GroupDialog component

--- client/src/components/group_dialog/GroupDialog.js
+++ client/src/components/group_dialog/GroupDialog.js
@@ -1,36 +1,72 @@
 import React from 'react'
 import Dialog from 'material-ui/Dialog'
 import IconButton from '../icon_button/IconButton'
+import TextField from 'material-ui/TextField'
+import Icon from '../icon/Icon'
+import IconicButton from 'material-ui/IconButton'
+import './group_dialog.css'
 
 export default class GroupDialog extends React.Component {
   static propTypes = {
     open: React.PropTypes.bool.isRequired,
     onClose: React.PropTypes.func.isRequired,
     onAccept: React.PropTypes.func.isRequired,
-    children: React.PropTypes.node.isRequired,
+  }
+
+  state = {
+    screenName: '',
+  }
+
+  handleScreenNameChange = (event, newValue) => {
+    this.setState({
+      screenName: newValue,
+    })
+  }
+
+  handleScreenNameReset = () => {
+    this.setState({
+      screenName: '',
+    })
+  }
+
+  handleClose = () => {
+    this.handleScreenNameReset()
+    this.props.onClose()
   }
 
   handleAccept = () => {
-    this.props.onClose()
-    this.props.onAccept()
+    const screenName = this.state.screenName
+    this.handleClose()
+
+    if (screenName.length > 0) {
+      this.props.onAccept(screenName)
+    }
   }
 
   render() {
     return <Dialog
       title="Добавить группу"
       open={this.props.open}
-      onRequestClose={this.props.onClose}
+      onRequestClose={this.handleClose}
       actions={[
         <IconButton
           icon="close"
           text="Отмена"
-          onClick={this.props.onClose} />,
+          onClick={this.handleClose} />,
         <IconButton
           icon="add"
           text="Добавить"
           onClick={this.handleAccept} />,
       ]}>
-      {this.props.children}
+      <TextField
+        className="GroupDialog-text-field"
+        floatingLabelText="Короткий адрес"
+        value={this.state.screenName}
+        fullWidth={true}
+        onChange={this.handleScreenNameChange} />
+      <IconicButton onTouchTap={this.handleScreenNameReset}>
+        <Icon name="close" />
+      </IconicButton>
     </Dialog>
   }
 }
