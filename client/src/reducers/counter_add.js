01acd2a9da9d851ab8293efd007da3c5e85a8a52
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-03-09 22:03:42 +0300

    Add counter add reducers

---
+++ client/src/reducers/counter_add.js
@@ -0,0 +1,30 @@
+import {COUNTER_ADD_REQUEST, COUNTER_ADD_SUCCESS, COUNTER_ADD_FAILURE} from '../actions/counter_add'
+import {FETCHING_STATE, SUCCESS_STATE, FAILURE_STATE} from '../common/states'
+import {setCountersState, updateGroup, initialCollectionState} from './common'
+import {handleActions} from 'redux-actions'
+
+export const counterAdd = handleActions({
+  [COUNTER_ADD_REQUEST]: (state, action) => ({
+    ...state,
+    items: setCountersState(state.items, action.payload, FETCHING_STATE),
+  }),
+  [COUNTER_ADD_SUCCESS]: (state, action) => ({
+    ...state,
+    items: updateGroup(state.items, action.payload.group_id, group => ({
+      ...group,
+      counters: {
+        state: SUCCESS_STATE,
+        items: [{
+          ...action.payload.new_counter,
+          delta: group.counters.items.length !== 0
+            ? action.payload.new_counter.value - group.counters.items[0].value
+            : undefined,
+        }, ...group.counters.items],
+      },
+    })),
+  }),
+  [COUNTER_ADD_FAILURE]: (state, action) => ({
+    ...state,
+    items: setCountersState(state.items, action.payload, FAILURE_STATE),
+  }),
+}, initialCollectionState)
--- client/src/reducers/root.js
+++ client/src/reducers/root.js
@@ -3,10 +3,11 @@ import {groups} from './groups.js'
 import {groupRemove} from './group_remove.js'
 import {groupAdd} from './group_add.js'
 import {counters} from './counters.js'
+import {counterAdd} from './counter_add.js'
 import reduceReducers from 'reduce-reducers'
 import {combineReducers} from 'redux'
 
 export const rootReducer = combineReducers({
   groupDialog,
-  groups: reduceReducers(groups, groupRemove, groupAdd, counters),
+  groups: reduceReducers(groups, groupRemove, groupAdd, counters, counterAdd),
 })
