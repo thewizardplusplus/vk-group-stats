9723f44998ff38fb0722b31c04907d4d0d47ce49
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-03-09 23:28:12 +0300

    Add the DrivedUpdateButton container

---
+++ client/src/containers/drived_update_button/DrivedUpdateButton.js
@@ -0,0 +1,22 @@
+import {addCounter} from '../../actions/counter_add'
+import {connect} from 'react-redux'
+import UpdateButton from '../../components/update_button/UpdateButton'
+
+function mapStateToProps(state) {
+  return {
+    groupIds: state.groups.items.map(group => group.data._id),
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onClick(groupIds) {
+      groupIds.forEach(groupId => dispatch(addCounter(groupId)))
+    },
+  }
+}
+
+export const DrivedUpdateButton = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UpdateButton)
