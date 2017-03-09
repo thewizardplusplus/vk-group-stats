408b3eb53645072ebff8da5bc4e2b3a7c369b83c
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-03-09 20:31:43 +0300

    Use the DrivedCountersTable container

--- client/src/components/group/Group.js
+++ client/src/components/group/Group.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Card, CardHeader, CardText, CardActions} from 'material-ui/Card'
+import {DrivedCountersTable} from '../../containers/drived_counters_table/DrivedCountersTable'
 import IconButton from '../icon_button/IconButton'
 
 export default class Group extends React.Component {
   static propTypes = {
     group: React.PropTypes.shape({
+      _id: React.PropTypes.string.isRequired,
       screen_name: React.PropTypes.string.isRequired,
     }).isRequired,
     onRemove: React.PropTypes.func.isRequired,
@@ -14,7 +16,7 @@ export default class Group extends React.Component {
     return <Card>
       <CardHeader title={this.props.group.screen_name} />
       <CardText>
-        Dump text.
+        <DrivedCountersTable groupId={this.props.group._id} />
       </CardText>
       <CardActions>
         <IconButton
