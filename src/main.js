28a6248a3cda98eff906dffba0dcb21737505e27
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-03-18 20:20:18 +0300

    Revert "Issue #11: correct proxy settings"
    This reverts commit c8c5bf2eba86c155efe450164b0cfc59557cfaef.

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ init_mongodb(() => {
       extended: true,
     }))
     app.use(body_parser.json())
-    app.set('trust proxy', true) // trust a proxy
+    app.set('trust proxy', 1) // trust a first proxy
     app.use(session)
     app.use(passport.initialize())
     app.use(passport.session())
