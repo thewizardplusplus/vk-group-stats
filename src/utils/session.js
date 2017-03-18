c12abff9aef5c864c0b68b839fc44e3ab1be9b2c
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-03-18 20:34:45 +0300

    Issue #12: support secure cookies

--- src/utils/session.js
+++ src/utils/session.js
@@ -8,6 +8,9 @@ const session_secret = uuid_v4()
 export const cookie_parser = cookie_parser_generator(session_secret)
 const session_store = session_store_generator(session_generator)
 export const session = session_generator({
+  cookie: {
+    secure: true,
+  },
   name: 'sessionId',
   resave: false,
   saveUninitialized: false,
