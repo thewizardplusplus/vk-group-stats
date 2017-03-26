c028a1f4aee4c1416b9138b71ef6dfe5d227c289
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-03-26 06:20:58 +0300

    Issue #22: correct styles processing in the IconButton component

--- client/src/components/icon_button/IconButton.js
+++ client/src/components/icon_button/IconButton.js
@@ -3,10 +3,13 @@ import FlatButton from 'material-ui/FlatButton'
 import Icon from '../icon/Icon'
 
 export default class IconButton extends React.Component {
+  // this name required by the AppBar component for a correct styles processing
+  static muiName = 'FlatButton'
   static propTypes = {
     icon: React.PropTypes.string.isRequired,
     text: React.PropTypes.string.isRequired,
     type: React.PropTypes.oneOf(['primary', 'secondary', 'disabled']),
+    style: React.PropTypes.object,
     onClick: React.PropTypes.func.isRequired,
   }
 
@@ -17,6 +20,7 @@ export default class IconButton extends React.Component {
       primary={this.props.type === 'primary'}
       secondary={this.props.type === 'secondary'}
       disabled={this.props.type === 'disabled'}
+      style={this.props.style}
       onTouchTap={this.props.onClick} />
   }
 }
